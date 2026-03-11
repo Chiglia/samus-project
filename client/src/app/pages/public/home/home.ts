1a97f4216ab1557b2359e4efacf03a37f28ab485
Davide Chigliaro <davide.chigliaro@grupposcai.it>
2026-03-11 11:50:09 +0100

    Feat: better homepage

--- client/src/app/pages/public/home/home.ts
+++ client/src/app/pages/public/home/home.ts
@@ -1,11 +1,49 @@
 import { Component } from '@angular/core';
-import { Nav } from '../../../common-components/nav/nav';
 import { SharedModule } from '../../../../shared.module';
 
 @Component({
   selector: 'app-home',
-  imports: [SharedModule, Nav],
+  imports: [SharedModule],
   templateUrl: './home.html',
   styles: ``,
 })
-export class Home {}
+export class Home {
+  apps = [
+    {
+      name: 'Calendar',
+      icon: 'pi pi-calendar',
+      description: 'Plan events and collaborate with your team.',
+      link: '/calendar',
+    },
+    {
+      name: 'Notes',
+      icon: 'pi pi-file-edit',
+      description: 'Organize ideas and documents in one place.',
+      link: '/notes',
+    },
+    {
+      name: 'Contacts',
+      icon: 'pi pi-users',
+      description: 'Manage personal and professional contacts.',
+      link: '/contacts',
+    },
+    {
+      name: 'Drive',
+      icon: 'pi pi-cloud',
+      description: 'Store and share files securely.',
+      link: '/drive',
+    },
+    {
+      name: 'Tasks',
+      icon: 'pi pi-check-square',
+      description: 'Track tasks and productivity.',
+      link: '/tasks',
+    },
+    {
+      name: 'Mail',
+      icon: 'pi pi-envelope',
+      description: 'Private and secure email communication.',
+      link: '/mail',
+    },
+  ];
+}
